2d7133f54131c4d32ee044dad99d1deedb267325
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-03-04 18:14:35 +0500

    parser fixes

--- Utils/InputParser.cpp
+++ Utils/InputParser.cpp
@@ -15,15 +15,15 @@
 //        this->tokens.push_back(std::string(argv[i]));
 //}
 
-const std::string& InputParser::getCmdOption(const std::string &option) {
-    
-    std::vector<std::string>::const_iterator itr;
-    itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
-    if (itr != this->tokens.end() && ++itr != this->tokens.end()){
-        return *itr;
-    }
-    return empty_string;
-}
+//const std::string& InputParser::getCmdOption(const std::string &option) {
+//    
+//    std::vector<std::string>::const_iterator itr;
+//    itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
+//    if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+//        return *itr;
+//    }
+//    return empty_string;
+//}
 
 bool InputParser::cmdOptionExists(const std::string &option) {
     
