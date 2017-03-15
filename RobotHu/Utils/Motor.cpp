be68bd1e159b0f2f4a8a0a539d93338596156945
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-03-15 22:34:19 +0500

    uncomment bcm

--- RobotHu/Utils/Motor.cpp
+++ RobotHu/Utils/Motor.cpp
@@ -7,7 +7,7 @@
 //
 
 #include "Motor.hpp"
-//#include "bcm2835.h"
+#include "bcm2835.h"
 #include <stdexcept>
 #include <unistd.h>
 #include "../../Utils/Utils.hpp"
@@ -16,48 +16,48 @@ using namespace std;
 
 Motor::Motor(int enablePin, int stepPin, int dirPin) {
     
-//    if (!bcm2835_init()) {
-//        Utils::printError("bcm2835 can't be init");
-//        throw exception();
-//    }
-//    
-//    this->enablePin = enablePin;
-//    this->stepPin = stepPin;
-//    this->dirPin = dirPin;
-//    
-//    bcm2835_gpio_fsel(enablePin, BCM2835_GPIO_FSEL_OUTP);
-//    bcm2835_gpio_fsel(stepPin, BCM2835_GPIO_FSEL_OUTP);
-//    bcm2835_gpio_fsel(dirPin, BCM2835_GPIO_FSEL_OUTP);
-//    
-//    // Disable
-//    bcm2835_gpio_write(enablePin, LOW);
-//    bcm2835_gpio_write(enablePin, HIGH);
-//    
-//    // "Reset" step pin
-//    bcm2835_gpio_write(stepPin, LOW);
+    if (!bcm2835_init()) {
+        Utils::printError("bcm2835 can't be init");
+        throw exception();
+    }
+    
+    this->enablePin = enablePin;
+    this->stepPin = stepPin;
+    this->dirPin = dirPin;
+    
+    bcm2835_gpio_fsel(enablePin, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(stepPin, BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(dirPin, BCM2835_GPIO_FSEL_OUTP);
+    
+    // Disable
+    bcm2835_gpio_write(enablePin, LOW);
+    bcm2835_gpio_write(enablePin, HIGH);
+    
+    // "Reset" step pin
+    bcm2835_gpio_write(stepPin, LOW);
 }
 
 void Motor::enable() {
     
-    //bcm2835_gpio_write(enablePin, LOW);
+    bcm2835_gpio_write(enablePin, LOW);
     usleep(500);
 }
 
 void Motor::disable() {
     
-    //bcm2835_gpio_write(enablePin, HIGH);
+    bcm2835_gpio_write(enablePin, HIGH);
     usleep(500);
 }
 
 void Motor::setDirection(int direction) {
     
-    //bcm2835_gpio_write(dirPin, direction > 0 ? LOW : HIGH);
+    bcm2835_gpio_write(dirPin, direction > 0 ? LOW : HIGH);
     usleep(500);
 }
 
 void Motor::step() {
     
-    //bcm2835_gpio_write(stepPin, HIGH);
-    //bcm2835_gpio_write(stepPin, LOW);
+    bcm2835_gpio_write(stepPin, HIGH);
+    bcm2835_gpio_write(stepPin, LOW);
     usleep(500);
 }
