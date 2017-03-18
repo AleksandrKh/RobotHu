af566035be32ee6a4d67b07d753cf5af25185cbf
Aleksandr Khorobrykh <aleksandr.khorobrykh@gmail.com>
2017-03-18 16:56:21 +0500

    real test fixes

--- RobotHu/MotionController.cpp
+++ RobotHu/MotionController.cpp
@@ -81,11 +81,11 @@ void MotionController::move(MotionVector motionVector) {
     leftMotor.enable();
     rightMotor.enable();
     
-    if (fabs(motionVector.xzAngleInDeg) > 1) {
-        Utils::printMessage("XZ correction rotation at " + to_string(motionVector.xzAngleInDeg) + " degrees");
-        rotate(motionVector.xzAngleInDeg);
-        usleep(kInMovesTimeoutImMicrosec);
-    }
+//    if (fabs(motionVector.xzAngleInDeg) > 1) {
+//        Utils::printMessage("XZ correction rotation at " + to_string(motionVector.xzAngleInDeg) + " degrees");
+//        rotate(motionVector.xzAngleInDeg);
+//        usleep(kInMovesTimeoutImMicrosec);
+//    }
     
     if (!stopMotionShared) { // if no new motion while rotation being processed
         
